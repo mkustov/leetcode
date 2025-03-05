482cb866840d6806fa4bfdbf78c3049725491c9d
Maxim Kustov <maksym.kustov@gmail.com>
2025-03-05 23:02:11 +0000

    1606 @wip

---
+++ 1606_busiest_server.rb
@@ -0,0 +1,78 @@
+require 'pry'
+# @param {Integer} k
+# @param {Integer[]} arrival
+# @param {Integer[]} load
+# @return {Integer[]}
+def busiest_servers(k, arrival, load)
+  server_load = {}
+  current_load = {}
+  k.times do |i| 
+    server_load[i] = 0
+    current_load[i] = 0
+  end
+  
+  servers = Array.new(k) { |s| s }
+
+  arrival.each_with_index do |arrival_time, i|
+    p "*********ARRIVAL #{arrival_time}, LOAD #{load[i]}*********"
+    p servers: servers
+    server_to_pick_request = servers.shift
+
+    p server_to_pick_request: server_to_pick_request
+
+    if server_to_pick_request
+      current_load[server_to_pick_request] = load[i]
+      server_load[server_to_pick_request] += 1
+    end
+
+    p current_load: current_load
+    p server_load: server_load
+
+    p '-----'
+    p current_load: current_load
+    current_load.keys.each do |server|
+      
+      p server: server
+      p current_load_server_before: current_load[server]
+      if server != server_to_pick_request && current_load[server] > 0
+        prev_time = i >= 1 ? arrival[i-1] : 1
+        current_load[server] -= arrival_time - prev_time
+        current_load[server] = [current_load[server], 0].max
+        if current_load[server] == 0
+          servers << server
+        end
+      end
+      p current_load_server_after: current_load[server]
+    end
+  end
+
+  grouped_by_processed_load = server_load.group_by { |k, v| v }
+  max_requests = grouped_by_processed_load.keys.max
+  grouped_by_processed_load[max_requests].map(&:first)
+  # server_load
+end
+
+# Input: k = 3, arrival = [1,2,3,4,5], load = [5,2,3,3,3] 
+# k = 3
+# arrival = [1,2,3,4,5]
+# load = [5,2,3,3,3] 
+# Output: [1] 
+# Explanation: 
+# All of the servers start out available.
+# The first 3 requests are handled by the first 3 servers in order.
+# Request 3 comes in. Server 0 is busy, so it's assigned to the next available server, which is 1.
+# Request 4 comes in. It cannot be handled since all servers are busy, so it is dropped.
+# Servers 0 and 2 handled one request each, while server 1 handled two requests. Hence server 1 is the busiest server.
+
+
+
+k = 2
+arrival = [2,3,4,8]
+load = [3,2,4,3]
+
+
+# k = 3
+# arrival = [1,2,3,4]
+# load = [1,2,1,2]
+
+p busiest_servers(k, arrival, load)
